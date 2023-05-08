c10f06a4ffadee2193b1ceca9b10640f33044278
User <User@WIN-0C9P01JPG3K>
2023-05-08 21:44:04 +0530

    initial commit

---
+++ FoxmindedTest/src/Packege/test.java
@@ -0,0 +1,14 @@
+package Packege;
+
+public class test {
+
+	public test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
